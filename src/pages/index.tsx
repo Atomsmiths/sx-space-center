f15b49fd133dbd6d316ed39d43f98886f10fc6ae
Oluane <rochepauline6@gmail.com>
2021-11-15 17:21:10 +0100

    client fetching example with swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import useSWR from "swr";
+
+async function fetcher(url: string, query: string): Promise<void> {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+      return json.data;
+    });
+}
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR<any>(
+    ["/api/graphql", "{ nextMission { dateUnix } }"],
+    fetcher,
+  );
+
+  if (error) return <div>Failed to load</div>;
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div>
       <Head>
@@ -10,6 +40,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>SpaceX Space Center</h1>
+      {data ? <p>{data.nextMission[0].dateUnix}</p> : <p>Loading...</p>}
     </div>
   );
 };
