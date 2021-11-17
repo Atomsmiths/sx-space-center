f16b987e76d3a9d0cdc7c1acc0a3278645a1d864
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-11-17 13:08:48 +0100

    Countdown component

---
+++ src/components/countdown.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const UpcomingMissionCountdown: React.FC<{ unixDate: number }> = ({
+  unixDate,
+}) => {
+  const [currentTime, setCurrentTime] = React.useState(
+    Math.floor(Date.now() / 1000),
+  );
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(Math.floor(Date.now() / 1000));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const delta = Math.floor(unixDate - currentTime);
+  const seconds = delta % 60;
+  const minutes = Math.floor(delta / 60) % 60;
+  const hours = Math.floor(delta / 3600) % 24;
+  const days = Math.floor(delta / 86400);
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-32 text-center">
+        <p className="text-6xl">{String(days).padStart(2, "0")}</p>
+        <p>days</p>
+      </div>
+      <div className="w-32 text-center">
+        <p className="text-6xl">{String(hours).padStart(2, "0")} </p>
+        <p>hours</p>
+      </div>
+      <div className="w-32 text-center">
+        <p className="text-6xl">{String(minutes).padStart(2, "0")}</p>
+        <p>minutes</p>
+      </div>
+      <div className="w-32 text-center">
+        <p className="text-6xl">{String(seconds).padStart(2, "0")}</p>
+        <p>seconds</p>
+      </div>
+    </div>
+  );
+};
+
+export { UpcomingMissionCountdown };
