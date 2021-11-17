a789a494973f5330a4209bc814d0f4ea4b39e233
Oluane <rochepauline6@gmail.com>
2021-11-17 12:32:22 +0100

    added custom font + some basic rules for TW

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import React from "react";
 
+import "@src/design-system/global-style.css";
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: "media",
-  theme: {},
+  theme: {
+    fontFamily: {
+      nasalization: ["Nasalization"],
+    },
+    // We are redefining fontSizes here, because since we set 1rem to be equal to 10px
+    // in global-style.css, the ones provided by Tailwind were too small.
+    fontSize: {
+      xs: "1rem",
+      s: "1.2rem",
+      m: "1.4rem",
+      base: "1.6rem",
+      l: "1.8rem",
+      xl: "2rem",
+      "2xl": "2.2rem",
+    },
+  },
   variants: {
     extend: {},
   },
