7363e2093288291672ac3938ae32d0c159a76557
Oluane <rochepauline6@gmail.com>
2021-11-18 12:58:12 +0100

    added colors + improved font sizing in TW config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: "media",
@@ -8,13 +11,26 @@ module.exports = {
     // We are redefining fontSizes here, because since we set 1rem to be equal to 10px
     // in global-style.css, the ones provided by Tailwind were too small.
     fontSize: {
-      xs: "1rem",
       s: "1.2rem",
       m: "1.4rem",
       base: "1.6rem",
-      l: "1.8rem",
-      xl: "2rem",
-      "2xl": "2.2rem",
+      l: "2rem",
+      xl: "2.4rem",
+      "2xl": "2.8rem",
+      "3xl": "3.2rem",
+      "4xl": "3.6rem",
+      "5xl": "4rem",
+      "6xl": "4.6rem",
+      "7xl": "5rem",
+      "9xl": "8rem",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      eigengrau: "hsl(240, 14%, 10%);",
     },
   },
   variants: {
