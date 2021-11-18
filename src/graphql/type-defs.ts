cd317787c8ecd8740386d4f97d242cddd5ab92a8
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-11-18 13:04:29 +0100

    Improved graphql file design (#24)

---
+++ src/graphql/type-defs.ts
@@ -0,0 +1,15 @@
+import { gql } from "apollo-server-core";
+
+import launches from "./launches/type-defs";
+
+// We add a root type def where other queries will be extended from.
+// This is needed to fragment our typeDefs and resolvers.
+const root = gql`
+  type Query {
+    root: String
+  }
+`;
+
+const typeDefs = [root, launches];
+
+export { typeDefs };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import useSWR from "swr";
 
+import { UPCOMING_LAUNCH_QUERY } from "@src/graphql/launches/queries";
+
 async function fetcher(url: string, query: string): Promise<void> {
   return fetch(url, {
     method: "POST",
@@ -18,23 +20,9 @@ async function fetcher(url: string, query: string): Promise<void> {
     });
 }
 
-const UPCOMING_MISSION_QUERY = `
-  query upcomingMission {
-    upcomingMission {
-      name
-      dateUnix
-      rocket
-      patch {
-        small
-        large
-      }
-    }
-  }
-`;
-
 const Home: NextPage = () => {
   const { data, error } = useSWR<any>(
-    ["/api/graphql", UPCOMING_MISSION_QUERY],
+    ["/api/graphql", UPCOMING_LAUNCH_QUERY],
     fetcher,
   );
 
@@ -50,9 +38,9 @@ const Home: NextPage = () => {
       <h1>SpaceX Space Center</h1>
       {data ? (
         <div>
-          <p>{data.upcomingMission.name}</p>
-          <p>{data.upcomingMission.dateUnix}</p>
-          <p>{data.upcomingMission.rocket}</p>
+          <p>{data.upcomingLaunch.name}</p>
+          <p>{data.upcomingLaunch.dateUnix}</p>
+          <p>{data.upcomingLaunch.rocket}</p>
         </div>
       ) : (
         <p>Loading...</p>
