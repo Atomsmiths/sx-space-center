55280ba48c3adb930074bf789b86644af5f292e5
Oluane <rochepauline6@gmail.com>
2021-11-23 20:00:37 +0100

    cleaning test page

--- src/pages/testing/index.tsx
+++ src/pages/testing/index.tsx
@@ -2,22 +2,21 @@ import { request } from "graphql-request";
 import React from "react";
 import useSWR from "swr";
 
-// import { RocketFull } from "@src/@types/graphql-schema";
+import { RocketFull } from "@src/@types/graphql-schema";
 import { ONE_ROCKET } from "@src/graphql/rockets/queries";
 
-// type GraphQlOperation = {
-//   query: string;
-//   variables: Record<string, string>;
-// };
-
-function fetcher(url: string, query: string, variable: string): void {
-  // console.log("variables:", variable);
-  const data = request(url, query, { rocketId: variable });
-  console.log("data:", data);
+async function fetcher(
+  url: string,
+  query: string,
+  variable: string,
+): Promise<RocketFull> {
+  return await request(url, query, { rocketId: variable }).then((data) => {
+    return data.oneRocket;
+  });
 }
 
 const TestComponent: React.FC = () => {
-  const { data, error } = useSWR<void, Error>(
+  const { data, error } = useSWR<RocketFull, Error>(
     ["/api/graphql", ONE_ROCKET, "5e9d0d95eda69955f709d1eb"],
     fetcher,
   );
@@ -26,9 +25,11 @@ const TestComponent: React.FC = () => {
     return <div>Failed to load</div>;
   }
 
-  console.log(data);
+  if (!data) {
+    <div>Loading...</div>;
+  }
 
-  return <div>Nothing to see here.</div>;
+  return <div>{data ? data.name : null}</div>;
 };
 
 export default TestComponent;
