ba0310e8752e9e4e7e2671aacb6c6f1e2d4d6661
Pauline Roche <rochepauline6@gmail.com>
2021-11-24 10:57:28 +0100

    Add all rockets & one rocket by id queries (#37)
    * added queries + typedef+types+resolver for rockets
    
    * tets page
    
    * cleaning test page
    
    * fixing issue with queries arguments inside resolvers
    
    Co-authored-by: Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>

---
+++ src/graphql/rockets/type-defs.ts
@@ -0,0 +1,49 @@
+import { gql } from "apollo-server-micro";
+
+const rockets = gql`
+  extend type Query {
+    allRockets: [RocketPartial]
+    oneRocket(rocketId: String!): RocketFull
+  }
+
+  type RocketPartial {
+    name: String!
+    id: String!
+    imagesLinks: [String]!
+  }
+
+  type RocketSize {
+    meters: Float
+    feet: Float
+  }
+
+  type RocketMass {
+    kg: Float
+    lb: Float
+  }
+
+  type RocketEngines {
+    type: String
+    number: Int
+    propellants: [String]
+  }
+
+  type RocketFull {
+    name: String!
+    id: String!
+    imagesLinks: [String]!
+    height: RocketSize
+    diameter: RocketSize
+    mass: RocketMass
+    stages: Int
+    active: Boolean
+    costPerLaunch: Int
+    firstFlight: String
+    description: String
+    engines: RocketEngines
+    successRatePct: Int
+    wiki: String
+  }
+`;
+
+export default rockets;
