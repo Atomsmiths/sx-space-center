515dec110027b5d7a63e835e0ea250ea7cf08d23
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-11-24 13:33:04 +0100

    Improved headers global style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,9 +5,7 @@ import { Navbar } from "./navbar/navbar";
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="w-screen min-h-screen pt-20">
-      <h1 className="text-center tracking-widest text-4xl md:text-8xl">
-        SpaceX Space Center
-      </h1>
+      <h1 className="text-center tracking-widest">SpaceX Space Center</h1>
       <Navbar />
       <main>{children}</main>
     </div>
