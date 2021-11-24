5f65e2b3a588cff02493472280605afb1536c41f
Pauline Roche <rochepauline6@gmail.com>
2021-11-24 18:43:46 +0100

    Orbiting planet animation for Loading component (#42)
    * working solution for planet orbiting around another
    
    * cleaner version of orbiting planet loading component
    
    * element's size + anim duration adjustements
    
    * new loading component in homepage
    
    * PR amends
    
    * refacto
    
    * PR amends

---
+++ src/components/loading-component/loading-component.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import keys from "./loading-component.module.css";
+
+const LoadingComponent: React.FC = () => {
+  return (
+    <div id={keys.mainContainer} className="relative w-full">
+      <div
+        id={keys.centralStar}
+        className="absolute left-1/2 w-20 h-20 rounded-full bg-gray-100"
+      />
+      <div
+        id={keys.orbit}
+        className="absolute left-1/2 top-1/2 w-60 h-60 rounded-full"
+      >
+        <div id={keys.planetPos} className="absolute left-1/2 -top-px">
+          <div
+            id={keys.planet}
+            className="absolute left-1/2 right-1/2 w-6 h-6 rounded-full bg-gray-100 border-eigengrau border-solid"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { LoadingComponent };
