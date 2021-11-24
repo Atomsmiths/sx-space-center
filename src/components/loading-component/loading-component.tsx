fa846df58ad67da7bcdd4f67335b05beadfd3579
Oluane <rochepauline6@gmail.com>
2021-11-24 16:09:49 +0100

    refacto

--- src/components/loading-component/loading-component.tsx
+++ src/components/loading-component/loading-component.tsx
@@ -4,11 +4,20 @@ import keys from "./loading-component.module.css";
 
 const LoadingComponent: React.FC = () => {
   return (
-    <div id={keys.mainContainer}>
-      <div id={keys.centralStar}></div>
-      <div id={keys.orbit}>
-        <div id={keys.planetPos}>
-          <div id={keys.planet}></div>
+    <div id={keys.mainContainer} className="relative w-full">
+      <div
+        id={keys.centralStar}
+        className="absolute left-1/2 w-20 h-20 rounded-full bg-gray-100"
+      ></div>
+      <div
+        id={keys.orbit}
+        className="absolute left-1/2 top-1/2 w-60 h-60 rounded-full"
+      >
+        <div id={keys.planetPos} className="absolute left-1/2 -top-px">
+          <div
+            id={keys.planet}
+            className="absolute left-1/2 right-1/2 w-6 h-6 rounded-full bg-gray-100 border-eigengrau border-solid"
+          ></div>
         </div>
       </div>
     </div>
