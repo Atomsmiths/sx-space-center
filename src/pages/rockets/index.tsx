4f69778a3817e030d90d997955c94099b571213b
Oluane <rochepauline6@gmail.com>
2021-11-26 11:31:52 +0100

    WIP - rocket page

--- src/pages/rockets/index.tsx
+++ src/pages/rockets/index.tsx
@@ -1,8 +1,9 @@
 import { request } from "graphql-request";
-import Image from "next/image";
+// import Image from "next/image";
 import React from "react";
 import useSWR from "swr";
 
+import classes from "./index.module.css";
 import { RocketPartial } from "@src/@types/graphql-schema";
 import { ALL_ROCKETS } from "@src/graphql/rockets/queries";
 
@@ -25,19 +26,22 @@ const RocketsOverview: React.FC = () => {
   console.log(data);
 
   return (
-    <div className="">
+    <div>
       <h2 className="text-center">Rockets</h2>
-      <div className=" w-full flex flex-wrap pt-20 px-10">
+      <div className=" w-full flex flex-wrap pt-20 px-20">
         {data ? (
           data.map(({ name, id, imagesLinks }) => {
             return (
-              <div key={id} className="w-1/2 relative">
-                {name}
-                <Image
+              <div key={id} className={`w-1/2 pr-24 pb-24 ${classes.images}`}>
+                {/* <Image
                   src={imagesLinks[0] ? imagesLinks[0] : ""}
                   alt={`${name} image`}
                   width="500"
                   height="400"
+                /> */}
+                <img
+                  src={imagesLinks[0] ? imagesLinks[0] : ""}
+                  alt={`${name} image`}
                 />
               </div>
             );
