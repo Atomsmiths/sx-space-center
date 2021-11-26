c661485fcee25dfc12417e15ff6cb51941be2ec3
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-11-26 17:32:06 +0100

    Improved h1 css rules

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "./navbar/navbar";
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="w-screen min-h-screen pt-20">
-      <h1 className="text-center tracking-widest">SpaceX Space Center</h1>
+      <h1>SpaceX Space Center</h1>
       <Navbar />
       <main>{children}</main>
     </div>
