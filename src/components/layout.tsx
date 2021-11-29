113fac14d56abe7fe8f5ce9e9116759657292193
Pauline Roche <rochepauline6@gmail.com>
2021-11-29 13:43:25 +0100

    starry background v1 with css-doodle (#58)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import { Background } from "./background";
 import { Navbar } from "./navbar/navbar";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <div className="w-screen min-h-screen pt-20">
-      <h1>SpaceX Space Center</h1>
-      <Navbar />
-      <main className="pt-14 md:pt-20">{children}</main>
-    </div>
+    <>
+      <Background />
+      <div className="w-screen min-h-screen pt-20">
+        <h1>SpaceX Space Center</h1>
+        <Navbar />
+        <main className="pt-14 md:pt-20">{children}</main>
+      </div>
+    </>
   );
 };
 
