8f0f067b25601fc6f9e56fe1dde9165405835644
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-11-30 11:20:37 +0100

    Added text underline on active path link

--- src/components/navbar/navbar-mobile.tsx
+++ src/components/navbar/navbar-mobile.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/dist/client/router";
 import React from "react";
 
 import { SpaceRocket } from "../icons/space-rocket";
@@ -5,6 +6,7 @@ import { NavLink } from "../nav-link";
 
 const NavbarMobile: React.FC = () => {
   const [isMobileNavbarOpen, setIsMobileNavbarOpen] = React.useState(false);
+  const { pathname } = useRouter();
 
   return (
     <div className="fixed bottom-0 left-0 md:-left-4 w-full h-full md:hidden">
@@ -19,19 +21,39 @@ const NavbarMobile: React.FC = () => {
         } transition-transform duration-500 ease-in-out fixed w-full h-full bg-eigengrau z-10 flex flex-col justify-center`}
       >
         <div className="flex flex-col justify-around text-center h-2/3 text-2xl">
-          <NavLink href="/" onClick={() => setIsMobileNavbarOpen(false)}>
+          <NavLink
+            href="/"
+            onClick={() => setIsMobileNavbarOpen(false)}
+            classNames={pathname === "/" ? "underline" : ""}
+          >
             Home
           </NavLink>
-          <NavLink href="/future" onClick={() => setIsMobileNavbarOpen(false)}>
+          <NavLink
+            href="/future"
+            onClick={() => setIsMobileNavbarOpen(false)}
+            classNames={pathname === "/future" ? "underline" : ""}
+          >
             Future
           </NavLink>
-          <NavLink href="/past" onClick={() => setIsMobileNavbarOpen(false)}>
+          <NavLink
+            href="/past"
+            onClick={() => setIsMobileNavbarOpen(false)}
+            classNames={pathname === "/past" ? "underline" : ""}
+          >
             Past
           </NavLink>
-          <NavLink href="/history" onClick={() => setIsMobileNavbarOpen(false)}>
+          <NavLink
+            href="/history"
+            onClick={() => setIsMobileNavbarOpen(false)}
+            classNames={pathname === "/history" ? "underline" : ""}
+          >
             History
           </NavLink>
-          <NavLink href="/rocket" onClick={() => setIsMobileNavbarOpen(false)}>
+          <NavLink
+            href="/rocket"
+            onClick={() => setIsMobileNavbarOpen(false)}
+            classNames={pathname === "/rockets" ? "underline" : ""}
+          >
             Rocket
           </NavLink>
         </div>
