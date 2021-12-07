f31043571f44b68c62b80f2df3177f6dfa1aa560
Oluane <rochepauline6@gmail.com>
2021-12-07 19:11:29 +0100

    added generic to `merge.all()` to fix issue with build

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,8 +3,9 @@ import merge from "deepmerge";
 import { historyResolvers } from "./history/resolvers";
 import { launchesResolvers } from "./launches/resolvers";
 import { rocketsResolvers } from "./rockets/resolvers";
+import { ResolversTypes } from "@src/@types/graphql-schema";
 
-const resolvers = merge.all([
+const resolvers = merge.all<ResolversTypes>([
   launchesResolvers,
   rocketsResolvers,
   historyResolvers,
