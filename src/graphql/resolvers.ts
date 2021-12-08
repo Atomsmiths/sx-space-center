1f64f56dbe1aa1c8080556edd6923935265b0356
Oluane <rochepauline6@gmail.com>
2021-12-08 12:35:42 +0100

    fixing merge all resolvers type

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -3,9 +3,9 @@ import merge from "deepmerge";
 import { historyResolvers } from "./history/resolvers";
 import { launchesResolvers } from "./launches/resolvers";
 import { rocketsResolvers } from "./rockets/resolvers";
-import { ResolversTypes } from "@src/@types/graphql-schema";
 
-const resolvers = merge.all<ResolversTypes>([
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const resolvers = merge.all<Record<string, any>>([
   launchesResolvers,
   rocketsResolvers,
   historyResolvers,
